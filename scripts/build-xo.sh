a564c8d839393d60d6e4dcaa4e25410f4e380bf0
Matthieu Ducorps <sr.ht.qualify064@passmail.net>
2026-07-04 17:58:36 +0900

    adding all entry points for the root, v5 and v6

--- scripts/build-xo.sh
+++ scripts/build-xo.sh
@@ -40,7 +40,9 @@ cat > packages/xo-server/xoahl.config.toml << 'EOF'
   key = '/opt/xo/xoahl.key'
 
 [http.mounts]
+'/' = '../xo-web/dist/'
 '/v5' = '../xo-web/dist/'
+'/v6' = '../../@xen-orchestra/web/dist/'
 
 [redis]
 uri = 'redis://127.0.0.1:6379/0'
